04a918adf31c106da526c12797df4eedf5c58677
SBC <seobc789@gmail.com>
2025-01-23 17:36:18 +0900

    PJT250123
    Basic C# Project

---
+++ Work/PJT250123/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PJT250123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int -> 정수
+            // float -> 실수
+            // char -> 유니코드 한글자 'A' -> 65
+            // +, -, *, /, %
+            // 대입문 '='
+
+            int korean = 10;
+            int mathmatics = 100;
+            int english = 50;
+
+            int sum = korean + mathmatics + english;
+            float average = (float)sum / 3.0f;
+            // average = 0; 여기에서 0만이 .0f를 안씀(자료형이 float 이면)
+            
+            Console.WriteLine(average);
+        }
+    }
+}
